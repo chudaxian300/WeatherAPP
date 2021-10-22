2a8bf9037fc5536c9c069a118f1a91ca0f7050a3
chudaxian300 <81302819+chudaxian300@users.noreply.github.com>
2021-10-22 16:48:44 +0800

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
     message.textContent = ''
-    fetch('http://localhost:3001/weather?city=' + location).then((res) => {
+    fetch('http://120.24.193.73:3001/weather?city=' + location).then((res) => {
         res.json().then((data) => {
             if (data.error) {
                 console.log(data.error)
